a907f51014cfc94e4045310b1a71c8779281b556
ryusei-fujisaki <fujisaki-ryusei@dmm.com>
2022-08-12 14:26:23 +0900

    [dev/issue6] main.goの修正

--- main.go
+++ main.go
@@ -5,8 +5,4 @@ import "fmt"
 func main() {
 	fmt.Println("hell world")
 	fmt.Println("hello world fujisaki")
-	fmt.Println("hello world ryusei")
-	fmt.Println("hello world DMM")
-	fmt.Println("hello world ryusei")
-	fmt.Println("hello world DMM")
 }
