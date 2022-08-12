5c87758dc8d7a94faa0d22046c2994280cb8f0fd
ryusei-fujisaki <fujisaki-ryusei@dmm.com>
2022-08-12 12:39:26 +0900

    [test]

--- main.go
+++ main.go
@@ -7,4 +7,6 @@ func main() {
 	fmt.Println("hello world fujisaki")
 	fmt.Println("hello world ryusei")
 	fmt.Println("hello world DMM")
+	fmt.Println("hello world ryusei")
+	fmt.Println("hello world DMM")
 }
