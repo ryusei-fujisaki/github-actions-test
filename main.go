7fabcd899db016b905c226c728d2a9d26300e2af
ryusei-fujisaki <fujisaki-ryusei@dmm.com>
2022-08-12 15:47:40 +0900

    [dev/issue10] main.goの修正z

--- main.go
+++ main.go
@@ -7,16 +7,9 @@ func main() {
 	fmt.Println("hello world fujisaki")
 	num := sub(1)
 	fmt.Println(num)
-	str := sub2()
-	fmt.Println(str)
 }
 
 func sub(a int) int {
 	a += 1
 	return a
-}
-
-func sub2() string {
-	b := "DMM-fujisaki"
-	return b
 }
